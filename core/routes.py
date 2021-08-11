132647c0b3c8f871640da480db0d801de1fa51e4
ZaenalMaestro <zaenal.official.1@gmail.com>
2021-08-11 16:40:40 +0800

    get data dosen dan mahasiswa dari database

--- core/routes.py
+++ core/routes.py
@@ -2,10 +2,12 @@ from threading import main_thread
 from flask import render_template, request, redirect, flash
 from flask.helpers import url_for
 from werkzeug.utils import secure_filename
-from .model import Dosen
+from .model import Dosen, Mahasiswa
 from core import app, db
 from .library.helper import ekstrakGambar, hash_file, allowed_file, upload_file
 import os
+
+from pprint import pprint
 
 # ================ prodi ================ 
 @app.route('/')
@@ -60,27 +62,34 @@ def upload():
 @app.route('/verifikasi', methods=['GET', 'POST'])
 def verifikasi():
    if request.method == 'POST':
+      stambuk = request.form['stambuk']     
       file = request.files['file-skripsi']
       filename = secure_filename(file.filename)
       upload_file('uploads/verifikasi', file)
          
       # ekstrak gambar dari file pdf
       nama_gambar = ekstrakGambar(os.path.join('uploads/verifikasi', filename))
-      hash_dosen = [
-         'df67008eb8c97b5140254191c5a27c61bc45dab1',
-         '6cc9bffba6b7f7df5f28f48e7db911df99b5e6e0'
-      ]
       daftar_hash = []
       for gambar in nama_gambar:
          daftar_hash.append(hash_file(gambar))
          os.remove(gambar)
       
+      # get data mahasiswa by stambuk
+      
+      # get data dosen
+      row = Dosen.query.filter_by(id_dosen=2).first()
+      hash_dosen = list()
+      hash_dosen.append({
+         'nama_dosen': row.nama_dosen,
+         'tanda_tangan_digital_1': row.tanda_tangan_digital_1,
+         'tanda_tangan_digital_2': row.tanda_tangan_digital_2
+      })
       ttd_cocok = 0
       for dosen in hash_dosen:
          for mhs in daftar_hash:
-            if dosen == mhs:
+            if dosen['tanda_tangan_digital_1'] == mhs or dosen['tanda_tangan_digital_2'] == mhs:
                ttd_cocok += 1
       
       return 'tanda tangan yang cocok adalah '+ str(ttd_cocok)
-      
-   return render_template('verifikasi/verifikasi.html')
+   rows = Mahasiswa.query.filter_by(stambuk='13020160099').first()
+   return render_template('verifikasi/verifikasi.html',rows=rows)
