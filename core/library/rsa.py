7ee3f9048d6a9792d91a773980a86bceef13c724
ZaenalMaestro <zaenal.official.1@gmail.com>
2021-08-28 11:25:53 +0800

    memperbaiki penamaan fungsi pada metode rsa

--- core/library/rsa.py
+++ core/library/rsa.py
@@ -2,7 +2,7 @@ p = 59
 q = 67
 
 n = p * q
-tau_n = (p-1) * (q-1)
+tautient_n = (p-1) * (q-1)
 
 def cek_bilangan_bulat(bilangan):
    bilangan = float(bilangan)
@@ -18,31 +18,32 @@ def cek_bilangan_bulat(bilangan):
    for i in angka[1]:
       return False if int(i) != 0 else True
          
-def faktor_bilangan(e):
-   faktor_bilangan = set()
+def faktor_pembagi_bilangan(e):
+   hasil = set()
    for i in range(1, (e+1)):
       if e % i == 0:
-         faktor_bilangan.add(i)
-   return faktor_bilangan
+         hasil.add(i)
+   return hasil
 
 def relatif_prima(a, b):
-   # fpb = faktor persekutuan terbesar
-   fpb = faktor_bilangan(a).intersection(faktor_bilangan(b))
+   # fpb = faktor persekutuan bersama
+   fpb = faktor_pembagi_bilangan(a).intersection(faktor_pembagi_bilangan(b))
    fpb = list(fpb)
    
-   # jika fpb == 1 relatif prima else tidak relatif prima
+   # jika fpb == 1 maka True
+   # jika fpb != 1 maka False
    return True if len(fpb) == 1 and fpb[0] == 1 else False
 
-def generate_e(tau_n):
-   for e in range(3, tau_n):
-      if relatif_prima(tau_n, e):
+def generate_e(tautient_n):
+   for e in range(3, tautient_n):
+      if relatif_prima(tautient_n, e):
          return e
       
-def generate_d(tau_n, e):
+def generate_d(tautient_n, e):
    k_value = 1
    hasil_akhir = 0
    while True:
-      hasil = (1 + (k_value * tau_n)) / e
+      hasil = (1 + (k_value * tautient_n)) / e
       if cek_bilangan_bulat(hasil):
          hasil_akhir = hasil
          break
@@ -52,9 +53,9 @@ def generate_d(tau_n, e):
 
 def enkripsi(plaintext):
    # private key (d, n)
-   global tau_n, n
-   e = generate_e(tau_n)
-   d = generate_d(tau_n, e)
+   global tautient_n, n
+   e = generate_e(tautient_n)
+   d = generate_d(tautient_n, e)
    
    # convert plaintext ke ASCII
    nilai_ascii = []
@@ -70,9 +71,9 @@ def enkripsi(plaintext):
 
 def dekripsi(chipertext):
    # public key (e, n)
-   global tau_n, n
+   global tautient_n, n
    # nilai dekripsi
-   e = generate_e(tau_n)
+   e = generate_e(tautient_n)
    plaintext = []
    
    for chiper in chipertext:
